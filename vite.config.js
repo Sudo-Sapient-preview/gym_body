dcc162f1a92c252e03d4307a70886b33b8251319
Deepak-80085 <deepak.kumaran.m.10@gmail.com>
2026-03-11 16:28:05 +0530

    vercel

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        formcheck: resolve(__dirname, 'formcheck.html'),
+        setup: resolve(__dirname, 'setup.html'),
+        session: resolve(__dirname, 'session.html'),
+        summary: resolve(__dirname, 'summary.html')
+      }
+    }
+  },
   server: {
     host: 'localhost',
     port: 5173,
@@ -12,4 +24,3 @@ export default defineConfig({
     strictPort: true
   }
 });
-
